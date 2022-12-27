0c5921914a1d1117a31abc13bab65c60ab4e54a1
Phoom Punpeng <phoomp14@gmail.com>
2022-12-27 08:52:30 +0700

    Yolov5 training updates

---
+++ ocr/handwritten_aida/gen_yaml_from_list.py
@@ -0,0 +1,9 @@
+def main():
+    cls = ['\\lim_', 'u', '\\to', '7', '+', '\\cot', '-', '3', '\\frac', '4', 'v', '/', '5', '\\tan', '\\pi', '8', 'r', '\\infty', '\\sqrt', '1', '\\sin', 'y', '2', '\\cos', '\\sec', '\\left(', '6', '\\right)', 'b', '9', 'p', '0', 'x', 'e', '\\ln', 'd', 'h', 'z', 'n', 'w', 't', 'c', '\\csc', '\\log', 's', '=', 'k', 'g', '\\theta', '\\left|', '\\right|', 'a', '\\cdot', '.']
+
+    for i, ele in enumerate(cls):
+        print(f'{i}: \'{ele}\'')
+
+
+if __name__ == '__main__':
+    main()
