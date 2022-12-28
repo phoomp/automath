992b339c1b584cf56891f1e1001b1c3bfd58b44b
Phoom Punpeng <phoomp14@gmail.com>
2022-12-28 17:49:07 +0700

    training

--- formation/tests/rnn_gen_torch.py
+++ formation/tests/rnn_gen_torch.py
@@ -9,18 +9,97 @@ import torch
 import torch.nn
 from torch.utils.data import TensorDataset, DataLoader
 
+from tqdm import tqdm
+
 from utils import Model, Dataset
 
 
+TRAIN_SPLIT = 0.8
+
 parser = argparse.ArgumentParser()
+
+parser.add_argument('ds_path', type=str)
+parser.add_argument('seed', type=int)
+parser.add_argument('batch_size', type=int)
+
+
+def train(device, model, batch_size, train_loader, test_loader, max_seq_len, epochs, loss_fn, optimizer):
+    # BEGIN EPOCH
+    for epoch in range(epochs):
+        print(f'BEGIN EPOCH {epoch + 1} --------------------------------------------------------\n')
+        losses = []
+        # BEGIN TRAIN
+        hidden = model.init_state(device, batch_size)
+        for i, (x, y) in enumerate(tqdm(train_loader)):
+            x = x.to(device).long()
+            y = y.to(device).long()  # Already a torch.LongTensor()
+
+            # print(f'x: {x.size()}')
+            # print(f'y: {y.size()}')
+
+            y_pred, _ = model(x, hidden)
+            # y_pred = model(x)
+            y_pred = torch.transpose(y_pred, 1, 2)
+            # y_pred = torch.transpose(y_pred, 1, 1)
+
+            # print(y.size())
+            # print(y_pred.size())
+            loss = loss_fn(y_pred, y)
+            losses.append(loss.item())
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print(f'Train Loss: {torch.tensor(losses).mean():.3f}')
+        # END TRAIN
+
+        losses = []
+
+        # BEGIN VALIDATION
+        hidden = model.init_state(device, batch_size)
+        print('Validation...')
+        for i, (x, y) in enumerate(tqdm(test_loader)):
+            x = x.to(device).long()
+            y = y.to(device).long()
+
+            y_pred, _ = model(x, hidden)
+            y_pred = torch.transpose(y_pred, 1, 2)
+            # y_pred = model(x)
+            losses.append(loss_fn(y_pred, y).item())
+
+        print(f'Validation Loss: {torch.tensor(losses).mean():.3f}')
 
 
 def main():
-    ds = Dataset('alllines.txt')
+    args = parser.parse_args()
+    ds_path = args.ds_path
+    seed = args.seed
+    batch_size = args.batch_size
+
+    ds = Dataset(ds_path)
+    train_size = round(len(ds) * TRAIN_SPLIT)
+    val_size = len(ds) - train_size
+    train_ds, val_ds = torch.utils.data.random_split(ds, [train_size, val_size], generator=torch.Generator().manual_seed(seed))
+    train_loader = DataLoader(train_ds, batch_size=batch_size, drop_last=True)
+    val_loader = DataLoader(val_ds, batch_size=batch_size, drop_last=True)
 
     print(f'Length of dataset: {len(ds)}')
 
-    print(f'Getitem: {ds.__getitem__(0)}')
+    # Define our device
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    # Create our model
+    model = Model(ds).to(device)
+
+    # Loss and Optimizer
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+
+    # Train
+    train(device, model, batch_size, train_loader, val_loader, ds.max_seq_len, 10, loss_fn, optimizer)
+
+    # Try out the model
 
 
 if __name__ == '__main__':
--- formation/tests/utils.py
+++ formation/tests/utils.py
@@ -9,37 +9,39 @@ import numpy as np
 
 
 class Model(nn.Module):
-    def __init__(self, dataset, lstm_size, embedding_dim, lstm_num_layer, lstm_dropout=0.2):
+    def __init__(self, dataset, lstm_size=128, embedding_dim=128, lstm_num_layer=5, lstm_dropout=0.2):
         super().__init__()
-        self.lstm_size = 128
+        self.dataset = dataset
+        self.lstm_size = lstm_size
         self.embedding_dim = embedding_dim
         self.lstm_num_layer = lstm_num_layer
         self.lstm_dropout = lstm_dropout
 
-        n_vocab = dataset.get_vocab_len()
+        n_vocab = self.dataset.get_vocab_len()
 
         self.embedding1 = nn.Embedding(num_embeddings=n_vocab,
                                        embedding_dim=self.embedding_dim
                                        )
 
-        self.lstm = nn.LSTM(input_size=self.lstm_size,
+        self.lstm = nn.LSTM(input_size=self.embedding_dim,
                             hidden_size=self.lstm_size,
                             num_layers=self.lstm_num_layer,
-                            dropout=self.lstm_dropout
+                            dropout=self.lstm_dropout, batch_first=True
                             )
 
         self.fc1 = nn.Linear(self.lstm_size, n_vocab)
 
-    def forward(self, x, cell_state):
+    def forward(self, x, hidden):
         x = self.embedding1(x)
-        x, state = self.lstm(x, cell_state)
-        x = self.fc(x)
-        return x, state
+        # print(f'Before error: {x.size()}, {hidden[0].size()}')
+        x, hidden = self.lstm(x, hidden)
+        x = self.fc1(x)
+        return x, hidden
 
-    def init_state(self, seq_len):
+    def init_state(self, device, batch_size):
         return (
-            torch.zeros(self.lstm_num_layer, seq_len, self.lstm_size),
-            torch.zeros(self.lstm_num_layer, seq_len, self.lstm_size)
+            torch.randn(self.lstm_num_layer, batch_size, self.lstm_size).to(device),
+            torch.randn(self.lstm_num_layer, batch_size, self.lstm_size).to(device),
         )
 
 
@@ -65,8 +67,8 @@ class Dataset(torch.utils.data.Dataset):
                 line = line.replace('\n', '')
                 self.lines.append(line)
 
-        print(len(self.lines))
-        print(self.lines[-1])
+        # print(len(self.lines))
+        # print(self.lines[-1])
 
         # Prepare dataset
         self.s_lines = [list(s) for s in self.lines]
@@ -75,7 +77,7 @@ class Dataset(torch.utils.data.Dataset):
 
         # Build vocabs
         v = sorted(set(''.join(self.lines)))
-        self.vocab = {}
+        self.vocab = {'EOS': 0}
         for char in v:
             self.vocab[char] = len(self.vocab)
 
@@ -84,21 +86,43 @@ class Dataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.s_lines)
 
-    def get_tokenized_tensor(self, line):
-        tensor = torch.zeros(len(line) - 1, 1, len(self.vocab))
+    def get_tokenized_tensor_onehot(self, line):
+        tensor = torch.zeros(self.max_seq_len, len(self.vocab))
 
-        for i, char in enumerate(line[:-1]):
+        for i, char in enumerate(line):
             idx = self.vocab[char]
-            tensor[i, 0, idx] = 1
+            tensor[i, idx] = 1
 
         return tensor
 
-    def get_tokenized_label(self, x):
-        tensor = torch.zeros()
+    def get_tokenized_tensor(self, line):
+        new_list = []
+
+        for i, char in enumerate(line):
+            new_list.append(self.vocab[char])
+
+        while len(new_list) < self.max_seq_len:
+            new_list.append(0)
+
+        return torch.Tensor(new_list).float()
+
+    def get_tokenized_label(self, line):
+        new_line = []
+        for i, char in enumerate(line[1:]):
+            new_line.append(self.vocab[char])
+
+        new_line.append(self.vocab['EOS'])
+        while len(new_line) < self.max_seq_len:
+            new_line.append(0)
+        # print(new_line)
+        return torch.Tensor(new_line).float()
+
+    def get_vocab_len(self):
+        return len(self.vocab)
 
     def __getitem__(self, idx):
         selected = self.s_lines[idx]
-        feature = torch.Tensor(self.get_tokenized_tensor(selected))
-        labels = torch.Tensor(self.get_tokenized_label(selected))
+        feature = self.get_tokenized_tensor(selected)
+        label = self.get_tokenized_label(selected)
 
-        return feature, labels
+        return feature, label
